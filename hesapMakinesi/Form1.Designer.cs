1a7a40969dc9fe9d4f3ed8d2d983ab5106adbdd6
mami <mami@DESKTOP-09J5HEI>
2023-09-13 02:00:00 +0300

    Proje dosyası ekle.

---
+++ hesapMakinesi/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace hesapMakinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSayi1 = new System.Windows.Forms.Button();
+            this.btnSayi2 = new System.Windows.Forms.Button();
+            this.btnSayi3 = new System.Windows.Forms.Button();
+            this.btnSayi6 = new System.Windows.Forms.Button();
+            this.btnSayi5 = new System.Windows.Forms.Button();
+            this.btnSayi4 = new System.Windows.Forms.Button();
+            this.btnSayi9 = new System.Windows.Forms.Button();
+            this.btnSayi8 = new System.Windows.Forms.Button();
+            this.btnSayi7 = new System.Windows.Forms.Button();
+            this.btnIslemSonuc = new System.Windows.Forms.Button();
+            this.btnSayi0 = new System.Windows.Forms.Button();
+            this.btnIslemToplama = new System.Windows.Forms.Button();
+            this.btnIslemCikartma = new System.Windows.Forms.Button();
+            this.btnIslemCarpim = new System.Windows.Forms.Button();
+            this.btnIslemBolum = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.anaTextBox = new System.Windows.Forms.TextBox();
+            this.topTextBox = new System.Windows.Forms.TextBox();
+            this.btnBackspace = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSayi1
+            // 
+            this.btnSayi1.Location = new System.Drawing.Point(23, 117);
+            this.btnSayi1.Name = "btnSayi1";
+            this.btnSayi1.Size = new System.Drawing.Size(69, 62);
+            this.btnSayi1.TabIndex = 0;
+            this.btnSayi1.Text = "1";
+            this.btnSayi1.UseVisualStyleBackColor = true;
+            this.btnSayi1.Click += new System.EventHandler(this.btnSayi1_Click);
+            // 
+            // btnSayi2
+            // 
+            this.btnSayi2.Location = new System.Drawing.Point(98, 117);
+            this.btnSayi2.Name = "btnSayi2";
+            this.btnSayi2.Size = new System.Drawing.Size(69, 62);
+            this.btnSayi2.TabIndex = 1;
+            this.btnSayi2.Text = "2";
+            this.btnSayi2.UseVisualStyleBackColor = true;
+            this.btnSayi2.Click += new System.EventHandler(this.btnSayi2_Click);
+            // 
+            // btnSayi3
+            // 
+            this.btnSayi3.Location = new System.Drawing.Point(173, 117);
+            this.btnSayi3.Name = "btnSayi3";
+            this.btnSayi3.Size = new System.Drawing.Size(69, 62);
+            this.btnSayi3.TabIndex = 2;
+            this.btnSayi3.Text = "3";
+            this.btnSayi3.UseVisualStyleBackColor = true;
+            this.btnSayi3.Click += new System.EventHandler(this.btnSayi3_Click);
+            // 
+            // btnSayi6
+            // 
+            this.btnSayi6.Location = new System.Drawing.Point(173, 185);
+            this.btnSayi6.Name = "btnSayi6";
+            this.btnSayi6.Size = new System.Drawing.Size(69, 62);
+            this.btnSayi6.TabIndex = 5;
+            this.btnSayi6.Text = "6";
+            this.btnSayi6.UseVisualStyleBackColor = true;
+            this.btnSayi6.Click += new System.EventHandler(this.btnSayi6_Click);
+            // 
+            // btnSayi5
+            // 
+            this.btnSayi5.Location = new System.Drawing.Point(98, 185);
+            this.btnSayi5.Name = "btnSayi5";
+            this.btnSayi5.Size = new System.Drawing.Size(69, 62);
+            this.btnSayi5.TabIndex = 4;
+            this.btnSayi5.Text = "5";
+            this.btnSayi5.UseVisualStyleBackColor = true;
+            this.btnSayi5.Click += new System.EventHandler(this.btnSayi5_Click);
+            // 
+            // btnSayi4
+            // 
+            this.btnSayi4.Location = new System.Drawing.Point(23, 185);
+            this.btnSayi4.Name = "btnSayi4";
+            this.btnSayi4.Size = new System.Drawing.Size(69, 62);
+            this.btnSayi4.TabIndex = 3;
+            this.btnSayi4.Text = "4";
+            this.btnSayi4.UseVisualStyleBackColor = true;
+            this.btnSayi4.Click += new System.EventHandler(this.btnSayi4_Click);
+            // 
+            // btnSayi9
+            // 
+            this.btnSayi9.Location = new System.Drawing.Point(173, 253);
+            this.btnSayi9.Name = "btnSayi9";
+            this.btnSayi9.Size = new System.Drawing.Size(69, 62);
+            this.btnSayi9.TabIndex = 8;
+            this.btnSayi9.Text = "9";
+            this.btnSayi9.UseVisualStyleBackColor = true;
+            this.btnSayi9.Click += new System.EventHandler(this.btnSayi9_Click);
+            // 
+            // btnSayi8
+            // 
+            this.btnSayi8.Location = new System.Drawing.Point(98, 253);
+            this.btnSayi8.Name = "btnSayi8";
+            this.btnSayi8.Size = new System.Drawing.Size(69, 62);
+            this.btnSayi8.TabIndex = 7;
+            this.btnSayi8.Text = "8";
+            this.btnSayi8.UseVisualStyleBackColor = true;
+            this.btnSayi8.Click += new System.EventHandler(this.btnSayi8_Click);
+            // 
+            // btnSayi7
+            // 
+            this.btnSayi7.Location = new System.Drawing.Point(23, 253);
+            this.btnSayi7.Name = "btnSayi7";
+            this.btnSayi7.Size = new System.Drawing.Size(69, 62);
+            this.btnSayi7.TabIndex = 6;
+            this.btnSayi7.Text = "7";
+            this.btnSayi7.UseVisualStyleBackColor = true;
+            this.btnSayi7.Click += new System.EventHandler(this.btnSayi7_Click);
+            // 
+            // btnIslemSonuc
+            // 
+            this.btnIslemSonuc.Location = new System.Drawing.Point(173, 321);
+            this.btnIslemSonuc.Name = "btnIslemSonuc";
+            this.btnIslemSonuc.Size = new System.Drawing.Size(69, 62);
+            this.btnIslemSonuc.TabIndex = 11;
+            this.btnIslemSonuc.Text = "=";
+            this.btnIslemSonuc.UseVisualStyleBackColor = true;
+            this.btnIslemSonuc.Click += new System.EventHandler(this.btnIslemSonuc_Click);
+            // 
+            // btnSayi0
+            // 
+            this.btnSayi0.Location = new System.Drawing.Point(98, 321);
+            this.btnSayi0.Name = "btnSayi0";
+            this.btnSayi0.Size = new System.Drawing.Size(69, 62);
+            this.btnSayi0.TabIndex = 10;
+            this.btnSayi0.Text = "0";
+            this.btnSayi0.UseVisualStyleBackColor = true;
+            this.btnSayi0.Click += new System.EventHandler(this.btnSayi0_Click);
+            // 
+            // btnIslemToplama
+            // 
+            this.btnIslemToplama.Location = new System.Drawing.Point(248, 321);
+            this.btnIslemToplama.Name = "btnIslemToplama";
+            this.btnIslemToplama.Size = new System.Drawing.Size(69, 62);
+            this.btnIslemToplama.TabIndex = 15;
+            this.btnIslemToplama.Text = "+";
+            this.btnIslemToplama.UseVisualStyleBackColor = true;
+            this.btnIslemToplama.Click += new System.EventHandler(this.btnIslemToplama_Click);
+            // 
+            // btnIslemCikartma
+            // 
+            this.btnIslemCikartma.Location = new System.Drawing.Point(248, 253);
+            this.btnIslemCikartma.Name = "btnIslemCikartma";
+            this.btnIslemCikartma.Size = new System.Drawing.Size(69, 62);
+            this.btnIslemCikartma.TabIndex = 14;
+            this.btnIslemCikartma.Text = "-";
+            this.btnIslemCikartma.UseVisualStyleBackColor = true;
+            this.btnIslemCikartma.Click += new System.EventHandler(this.btnIslemCikartma_Click);
+            // 
+            // btnIslemCarpim
+            // 
+            this.btnIslemCarpim.Location = new System.Drawing.Point(248, 185);
+            this.btnIslemCarpim.Name = "btnIslemCarpim";
+            this.btnIslemCarpim.Size = new System.Drawing.Size(69, 62);
+            this.btnIslemCarpim.TabIndex = 13;
+            this.btnIslemCarpim.Text = "X";
+            this.btnIslemCarpim.UseVisualStyleBackColor = true;
+            this.btnIslemCarpim.Click += new System.EventHandler(this.btnIslemCarpim_Click);
+            // 
+            // btnIslemBolum
+            // 
+            this.btnIslemBolum.Location = new System.Drawing.Point(248, 117);
+            this.btnIslemBolum.Name = "btnIslemBolum";
+            this.btnIslemBolum.Size = new System.Drawing.Size(69, 62);
+            this.btnIslemBolum.TabIndex = 12;
+            this.btnIslemBolum.Text = "/";
+            this.btnIslemBolum.UseVisualStyleBackColor = true;
+            this.btnIslemBolum.Click += new System.EventHandler(this.btnIslemBolum_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Location = new System.Drawing.Point(23, 321);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(69, 62);
+            this.btnTemizle.TabIndex = 9;
+            this.btnTemizle.Text = "C";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // anaTextBox
+            // 
+            this.anaTextBox.Location = new System.Drawing.Point(23, 70);
+            this.anaTextBox.Multiline = true;
+            this.anaTextBox.Name = "anaTextBox";
+            this.anaTextBox.Size = new System.Drawing.Size(294, 41);
+            this.anaTextBox.TabIndex = 16;
+            // 
+            // topTextBox
+            // 
+            this.topTextBox.Location = new System.Drawing.Point(23, 31);
+            this.topTextBox.Name = "topTextBox";
+            this.topTextBox.Size = new System.Drawing.Size(198, 22);
+            this.topTextBox.TabIndex = 17;
+            // 
+            // btnBackspace
+            // 
+            this.btnBackspace.Location = new System.Drawing.Point(235, 21);
+            this.btnBackspace.Name = "btnBackspace";
+            this.btnBackspace.Size = new System.Drawing.Size(82, 42);
+            this.btnBackspace.TabIndex = 18;
+            this.btnBackspace.Text = "<<";
+            this.btnBackspace.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 402);
+            this.Controls.Add(this.btnBackspace);
+            this.Controls.Add(this.topTextBox);
+            this.Controls.Add(this.anaTextBox);
+            this.Controls.Add(this.btnIslemToplama);
+            this.Controls.Add(this.btnIslemCikartma);
+            this.Controls.Add(this.btnIslemCarpim);
+            this.Controls.Add(this.btnIslemBolum);
+            this.Controls.Add(this.btnIslemSonuc);
+            this.Controls.Add(this.btnSayi0);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnSayi9);
+            this.Controls.Add(this.btnSayi8);
+            this.Controls.Add(this.btnSayi7);
+            this.Controls.Add(this.btnSayi6);
+            this.Controls.Add(this.btnSayi5);
+            this.Controls.Add(this.btnSayi4);
+            this.Controls.Add(this.btnSayi3);
+            this.Controls.Add(this.btnSayi2);
+            this.Controls.Add(this.btnSayi1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSayi1;
+        private System.Windows.Forms.Button btnSayi2;
+        private System.Windows.Forms.Button btnSayi3;
+        private System.Windows.Forms.Button btnSayi6;
+        private System.Windows.Forms.Button btnSayi5;
+        private System.Windows.Forms.Button btnSayi4;
+        private System.Windows.Forms.Button btnSayi9;
+        private System.Windows.Forms.Button btnSayi8;
+        private System.Windows.Forms.Button btnSayi7;
+        private System.Windows.Forms.Button btnIslemSonuc;
+        private System.Windows.Forms.Button btnSayi0;
+        private System.Windows.Forms.Button btnIslemToplama;
+        private System.Windows.Forms.Button btnIslemCikartma;
+        private System.Windows.Forms.Button btnIslemCarpim;
+        private System.Windows.Forms.Button btnIslemBolum;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.TextBox anaTextBox;
+        private System.Windows.Forms.TextBox topTextBox;
+        private System.Windows.Forms.Button btnBackspace;
+    }
+}
+
---
+++ hesapMakinesi/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIslemToplama_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIslemCikartma_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIslemCarpim_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIslemBolum_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIslemSonuc_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSayi0_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSayi9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSayi8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSayi7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSayi6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSayi5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSayi4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSayi3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSayi2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSayi1_Click(object sender, EventArgs e)
+        {
+            anaTextBox.Text += "1";
+        }
+    }
+}
